b2e2e6b08e82aef8fe2f57690dbcc9a84cf411c4
Atan Tanujaya <110257348+AtanXrra@users.noreply.github.com>
2024-06-05 06:10:50 +0700

    Add files via upload

---
+++ marketplace2594.sql
@@ -0,0 +1,326 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 04, 2024 at 09:48 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marketplace2594`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`) VALUES
+(1, 'admin@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Ngadimin, S.Kom');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE `artikel` (
+  `id_artikel` int(11) NOT NULL,
+  `judul_artikel` varchar(255) NOT NULL,
+  `isi_artikel` text NOT NULL,
+  `foto_artikel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `artikel`
+--
+
+INSERT INTO `artikel` (`id_artikel`, `judul_artikel`, `isi_artikel`, `foto_artikel`) VALUES
+(1, 'Lenovo Ideapad Gaming 3 Super', '<p>AMD Ryzen 7 5800H, 8GB DDR4, 512GB SSD M.2 NVMe, NVIDIA GeForce RTX, 15.6&quot; FHD (1920x1080), Color Shadow Black</p>\r\n', 'laptop_ideapad_3.jpg'),
+(2, 'Lenovo Gaming LOQ 15', '<p>I7 13620H 8GB 512 RTX4060 15.6&quot;WQHD IPS W11 Rp21,090,000 IDR* &middot; In stock &middot; Brand: Lenovo</p>\r\n', 'gaming_loq_15.jpg'),
+(3, 'ROG Strix G15', '<p>G513IH-R765B6T-O (AMD Ryzen 7, 8GB, 512GB SSD, GTX 1650) Images may be subject to copyright. Learn More</p>\r\n', 'rog_strix.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `foto_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `foto_kategori`) VALUES
+(5, 'Hewan', 'hewan1.jpg'),
+(6, 'Hape', 'iphone.jpg'),
+(7, 'Bola', 'bola.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `email_member` varchar(100) NOT NULL,
+  `password_member` varchar(100) NOT NULL,
+  `nama_member` varchar(100) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `wa_member` varchar(50) NOT NULL,
+  `kode_distrik_member` varchar(10) NOT NULL,
+  `nama_distrik_member` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `email_member`, `password_member`, `nama_member`, `alamat_member`, `wa_member`, `kode_distrik_member`, `nama_distrik_member`) VALUES
+(1, 'anolecrab@gmail.com', '399fb0159890e490c8b713c6348d03bf36896f0d', 'Anolecrab', 'Maguwoharjo Sleman   ', '081215778844    ', '512   ', 'Sleman   '),
+(2, 'xierra@gmail.com', '2d3ef12996f9559fffa6154d1f2b9d3b23bcac27', 'Xierra', 'Batu Kulon Progo', '081215778845', '36542', 'Kulon Progo'),
+(3, 'member@gmail.com', '6467baa3b187373e3931422e2a8ef22f3e447d77', 'Member', 'Batu Kulon Progo', '081215778845', '36542', 'Kulon Progo'),
+(4, 'flavio@gmail.com', '78425c32ad78109b2b4aa7b799549c63868ddfc5', 'Flavio', 'Pantai Indah Kapuk', ' 081215887754', '22', 'Kabupaten Bandung Jawa Barat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga_produk` int(11) NOT NULL,
+  `foto_produk` varchar(255) NOT NULL,
+  `deskripsi_produk` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `id_member`, `id_kategori`, `nama_produk`, `harga_produk`, `foto_produk`, `deskripsi_produk`) VALUES
+(7, 1, 5, 'Kucing', 1500000, 'kucing.jpeg', 'Kucing campur campur'),
+(8, 1, 6, 'Iphone 15 Pro ', 35000000, 'iphone_15_pro.jpg', 'Bagus'),
+(9, 1, 7, 'Bola Jabulani', 350000, 'bola_jabulani.jpg', 'Bola Keramat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `id_slider` int(11) NOT NULL,
+  `caption_slider` text NOT NULL,
+  `foto_slider` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`id_slider`, `caption_slider`, `foto_slider`) VALUES
+(1, 'Welcome to TechZone', 'Slider_-_1.png'),
+(2, 'Penjelasan TechZone', 'Slider_-_2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_member_beli` int(11) NOT NULL,
+  `id_member_jual` int(11) NOT NULL,
+  `tanggal_transaksi` datetime NOT NULL DEFAULT current_timestamp(),
+  `belanja_transaksi` int(11) NOT NULL,
+  `status_transaksi` enum('pesan','lunas','batal','dikirim','selesai') NOT NULL DEFAULT 'pesan',
+  `ongkir_transaksi` int(11) NOT NULL,
+  `total_transaksi` int(11) NOT NULL,
+  `bayar_transaksi` int(11) NOT NULL,
+  `distrik_pengirim` varchar(255) NOT NULL,
+  `nama_pengirim` varchar(100) NOT NULL,
+  `wa_pengirim` varchar(50) NOT NULL,
+  `alamat_pengirim` text NOT NULL,
+  `distrik_penerima` varchar(255) NOT NULL,
+  `nama_penerima` varchar(100) NOT NULL,
+  `wa_penerima` varchar(50) NOT NULL,
+  `alamat_penerima` text NOT NULL,
+  `nama_ekspedisi` varchar(100) NOT NULL,
+  `layanan_ekspedisi` varchar(100) NOT NULL,
+  `estimasi_ekspedisi` varchar(50) NOT NULL,
+  `berat_ekspedisi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_member_beli`, `id_member_jual`, `tanggal_transaksi`, `belanja_transaksi`, `status_transaksi`, `ongkir_transaksi`, `total_transaksi`, `bayar_transaksi`, `distrik_pengirim`, `nama_pengirim`, `wa_pengirim`, `alamat_pengirim`, `distrik_penerima`, `nama_penerima`, `wa_penerima`, `alamat_penerima`, `nama_ekspedisi`, `layanan_ekspedisi`, `estimasi_ekspedisi`, `berat_ekspedisi`) VALUES
+(1, 1, 2, '2024-03-19 15:35:34', 100000, 'pesan', 20000, 120000, 120000, 'Magelang', 'Iwuks', '081212554896', 'Pongangan no 1 Magelang', 'Candimulyo', 'Sendru', '081213335478', 'Candimulyo no 15 Magelang', 'JNE', 'Kilat', '1 Hari', '1000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `id_transaksi_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `nama_beli` varchar(255) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`id_transaksi_detail`, `id_transaksi`, `id_produk`, `nama_beli`, `harga_beli`, `jumlah_beli`) VALUES
+(1, 1, 1, 'Hijab Segi Empat', 50000, 1),
+(2, 1, 2, 'Mukena', 50000, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id_artikel`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id_slider`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`id_transaksi_detail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id_slider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  MODIFY `id_transaksi_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
